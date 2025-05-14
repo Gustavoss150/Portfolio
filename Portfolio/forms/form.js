aa62e6422be296c04b5f0110eb1868e8a18a6033
Gustavoss150 <gustavoss1@outlook.com>
2025-05-13 21:55:43 -0300

    inicio das funcoes js

--- Portfolio/forms/form.js
+++ Portfolio/forms/form.js
@@ -1,5 +1,71 @@
-function enviar() {
-    
+class Contato {
+    constructor() {
+        this.nome = null
+        this.email = null
+        this.fone = null
+        this.mensagem = null
+        this.data_cadastro = null
+        this.tipo_contato = null
+    }
+
+    // Métodos
+    Enviar() {
+        this.nome = document.getElementById('nome').value;
+        if (this.nome === '') {
+            alerta += ', Nome';
+        }
+        this.email = document.getElementById('email').value;
+        if (this.document === '') {
+            alerta += ', Email';
+        }
+        this.fone = document.getElementById('fone').value;
+        if (this.fone === '') {
+            alerta += ', Fone';
+        }
+        this.mensagem = document.getElementById('mensagem').value;
+        if (this.mensagem === '') {
+            alerta += ', Mensagem';
+        }
+        this.data_cadastro = new Date();
+        if (this.data_cadastro === '') {
+            alerta += ', Data Cadastro';
+        }
+        this.tipo_contato = document.getElementById('tipo_contato').value;
+        if (this.tipo_contato === '') {
+            alerta += ', Tipo Contato';
+        }
+
+        if (alerta != '') {
+            alert("Os seguintes campos estão com problemas: " +alerta)
+        }
+
+        let tabela = document.getElementById('dados');
+        let linha = tabela.insertRow();
+
+        linha.insertCell();
+        linha.innerHtml(this.nome);
+
+        linha.insertCell();
+        linha.innerHtml(this.email);
+
+        linha.insertCell();
+        linha.innerHtml(this.fone);
+
+        linha.insertCell();
+        linha.innerHtml(this.mensagem);
+
+        linha.insertCell();
+        linha.innerHtml(this.data_cadastro);
+
+        linha.insertCell();
+        linha.innerHtml(this.tipo_contato);
+    }
 }
-document.getElementById("nome").value
-let alerta = "aviso"
+
+contato = new Contato();
+/*
+function submeter(){
+    var alerta = document.getElementById('nome').value;
+    alert('Aviso: '+ alerta);
+}
+*/
